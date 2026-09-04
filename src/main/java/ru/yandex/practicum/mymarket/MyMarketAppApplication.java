21a2224442e44d1221630d751c9cef0282977398
Maksim Bagirov <maksim.bagirov@lenta.com>
2026-09-04 09:00:00 +0300

    build: set up Maven build and Spring Boot config

---
+++ src/main/java/ru/yandex/practicum/mymarket/MyMarketAppApplication.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.mymarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMarketAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMarketAppApplication.class, args);
+	}
+
+}
